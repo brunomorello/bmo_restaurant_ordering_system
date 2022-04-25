9fddde745a3f329c6611cbf3c755b041e880eeb5
Bruno Moreno <brunomorello7@gmail.com>
2022-04-25 16:22:43 +0100

    fix: Logged In user can see login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './authentication/authentication.guard';
+import { LoginGuard } from './authentication/login.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'  },
   // lazy loading
-  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard] },
   { path: 'orders', loadChildren: () => import('./orders/orders.module').then((m) => m.OrdersModule),
     canLoad: [AuthenticationGuard] }
 ];
