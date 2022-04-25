4fa20aae255c13cb9e5812a90d3f4becb1fa6431
Bruno Moreno <brunomorello7@gmail.com>
2022-04-25 17:23:36 +0100

    feat: created a component do display order details

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListOrdersComponent } from './list-orders/list-orders.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 const routes: Routes = [
-  { path: '', component: ListOrdersComponent }
+  { path: '', component: ListOrdersComponent },
+  { path: ':orderId', component: OrderDetailComponent }
 ];
 
 @NgModule({
